56aaacaad13ed04cf185840c5a95e99e158d1cf0
cattyproject <ivanilearningproject@gmail.com>
2022-01-09 12:20:55 +0700

    membuat sintaks sederhana pada script.js

--- CARD GAME WAR/deck.js
+++ CARD GAME WAR/deck.js
@@ -1,7 +1,7 @@
 const SUITS=["♠","♣","♥","♦"]
 const VALUES =["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
-class Deck {
+export default class Deck {
     constructor(cards=freshDeck()){
         this.cards=cards
     }
