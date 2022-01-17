b7b339901b27bd590dc92543e30137c9fed5f930
cattyproject <ivanilearningproject@gmail.com>
2022-01-17 11:58:39 +0700

    finish membuat logika game card

--- CARD GAME WAR/deck.js
+++ CARD GAME WAR/deck.js
@@ -9,6 +9,14 @@ export default class Deck {
     get numberOfCards(){
         return this.cards.length
     }
+    
+    pop(){
+        return this.cards.shift()
+    }
+
+    push(card){
+        this.cards.push(card)
+    }
     shuffle(){
         for( let i = this.numberOfCards - 1; i>0; i--){
             const newIndex = Math.floor(Math.random()*(i+1))
@@ -24,7 +32,19 @@ class Card {
         this.suit=suit
         this.value=value
     }
+    get color(){
+        return this.suit === '♣' || this.suit=== '♠' ? 'black' : 'red'
+    }
+    getHTML(){
+        const cardDiv=document.createElement('div')
+        cardDiv.innerText=this.suit
+        cardDiv.classList.add('card',this.color)
+        cardDiv.dataset.value=`${this.value} ${this.suit}`
+        return cardDiv
+    }
+
 }
+
 
 function freshDeck(){
     return SUITS.flatMap(suit =>{
